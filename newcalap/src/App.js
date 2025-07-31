eb5b8ace601444f4a14f8a7aa0162939d6963d75
EnesBasic <ssamss3004@gmail.com>
2025-07-31 22:13:47 +0200

    init

---
+++ newcalap/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { format, addMonths, subMonths } from 'date-fns';
+import Header from './components/Header';
+import CalendarGrid from './components/CalendarGrid';
+import './styles/global.css';
+
+function App() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  const handlePrevMonth = () => setCurrentDate(subMonths(currentDate, 1));
+  const handleNextMonth = () => setCurrentDate(addMonths(currentDate, 1));
+
+  return (
+    <div className="app">
+      <Header 
+        currentDate={currentDate} 
+        onPrevMonth={handlePrevMonth} 
+        onNextMonth={handleNextMonth} 
+      />
+      <CalendarGrid />
+    </div>
+  );
+}
+
+export default App;
